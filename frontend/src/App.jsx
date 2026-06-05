e169472319a20302c9db3b06d4f89b62a24d91cc
Matheus Henrique Domingos <mh.domingos@outlook.com>
2026-06-05 14:22:23 -0300

    Adiciona formulário de cadastro de pets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import './App.css'
 
 function App() {
   const [pets, setPets] = useState([])
+  const [nome, setNome] = useState("")
+  const [especie, setEspecie] = useState("")
 
-  useEffect(() => {
+  const buscarPets = () => {
   fetch("http://localhost:3000/pets")
     .then((response) => response.json())
     .then((data) => {
@@ -13,19 +15,63 @@ function App() {
     .catch((error) => {
       console.error("Erro ao buscar pets:", error)
     })
+}
+  useEffect(() => {
+  buscarPets()
 }, [])
 
   return (
     <div>
       <h1>Pet Adoption Platform</h1>
+<form
+  onSubmit={(event) => {
+    event.preventDefault()
+
+   const novoPet = {
+  nome,
+  especie
+}
+
+fetch("http://localhost:3000/pets", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(novoPet)
+})
+.then(() => {
+  buscarPets()
+})
+  }}
+>
+  <input
+    type="text"
+    value={nome}
+    onChange={(event) => setNome(event.target.value)}
+  />
+
+  <input
+  type="text"
+  value={especie}
+  onChange={(event) => setEspecie(event.target.value)}
+/>
+
+  <button type="submit">
+    Cadastrar
+  </button>
+</form>
+<p>Nome digitado: {nome}</p>
 
       <p>Quantidade de pets: {pets.length}</p>
 
       {
-       pets.map((pet) => (
-         <p key={pet.nome}>{pet.nome}</p>
-       ))
-    }
+  pets.map((pet) => (
+    <div key={pet._id}>
+      <h2>{pet.nome}</h2>
+      <p>Espécie: {pet.especie}</p>
+    </div>
+  ))
+}
   </div>
   )
 }
